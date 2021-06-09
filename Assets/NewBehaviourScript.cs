6406dd25f45684b7c13405f4638b2d509318eab1
Олег Бровченков <oleg.brovchenkov@gmail.com>
2021-06-09 18:21:19 +0300

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float x;
+    public Transform Camera;
+    private void Update()
+    {
+        x = Input.acceleration.y * 10;
+        print(x);
+        Camera.Rotate(Vector3.up * x);
+    }
+}
